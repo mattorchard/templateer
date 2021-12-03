fc4a0cfc3a1b1cf69164232c038c7051568f9fa1
Matt Orchard <morch028@uottawa.ca>
2021-12-02 19:28:08 -0500

    Quote strings as literals

--- src/lib/helpers/sqlValueHelpers.ts
+++ src/lib/helpers/sqlValueHelpers.ts
@@ -1,20 +1,20 @@
-const quoteLiteral = (text: string) => `'${text}'`
+const quoteLiteral = (text: string) => `'${text}'`;
 
-type FromJson = number | string | null | Array<FromJson> | {[key: string]: FromJson}
+type FromJson = number | string | null | Array<FromJson> | { [key: string]: FromJson };
 
 export const valueAsSql = (param: FromJson): string => {
 	if (param == null) {
-		return "null"
+		return 'null';
 	}
 	if (Array.isArray(param)) {
-		return `'{${param.join(", ")}}'`
+		return `'{${param.join(', ')}}'`;
 	}
 	switch (typeof param) {
-		case "number":
-			return param.toString()
-		case "string":
-			return param;
+		case 'number':
+			return param.toString();
+		case 'string':
+			return quoteLiteral(param);
 		case 'object':
 			return quoteLiteral(JSON.stringify(param));
 	}
-}
+};
